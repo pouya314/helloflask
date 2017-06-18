abf938238032b702a93c112409446b9d8ed5f22a
Pouya Arvandian <pouya314@gmail.com>
2017-06-18 20:50:26 +1000

    add ajax example

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, jsonify, render_template, request
 import sys
  
 
@@ -17,6 +17,25 @@ def add_entry():
         d.setdefault((key.split("_")[0] if ("_" in key) else key), []).append(value)
     print(d, file=sys.stdout)
     return render_template("ok.html")
+
+
+def add(first_number, second_number):
+    """This method adds 2 numbers."""
+    return first_number + second_number
+
+
+@app.route('/_add_numbers', methods = ["POST"])
+def add_numbers():
+    """Add two numbers server side, ridiculous but well..."""
+    a = int(request.json['a'])
+    b = int(request.json['b'])
+    return jsonify(result=add(a, b))
+
+
+@app.route('/show_jquery_example')
+def show_jquery_example():
+    return render_template('jquery_example.html')
+
 
 
 if __name__ == "__main__":
