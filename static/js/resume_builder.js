498ccab6cb3feecad6dc226b43f3236f91867a6c
Pouya Arvandian <pouya314@gmail.com>
2017-06-26 01:20:16 +1000

    add wrapper around each skill which makes it easier to add and remove them, also later for sorting

--- static/js/resume_builder.js
+++ static/js/resume_builder.js
@@ -9,15 +9,16 @@ $(function() {
 
     // page js
     document.addEventListener('click', function (e) {
+        e.preventDefault();
         if (hasClass(e.target, 'add-skill-link')) {
-            var idx = is_container_empty($("#skills")) ? -1 : parseInt($("#skills").find("input").last().attr("name").split("_").pop());
-            $("<input/>", {type: 'text', name: 'skill_'+(idx+1)}).appendTo('#skills');
-            $("<a/>", {text: 'x',class: 'remove-skill-link'}).appendTo('#skills');
-            $("<br/>").appendTo('#skills');
+            var idx = is_container_empty($("#skills")) ? -1 : parseInt($("#skills").find(".skill").last().find("input").attr("name").split("_").pop());
+            var skill_div = $("<div/>", {class: "skill"});
+            $("<input/>", {type: 'text', name: 'skill_'+(idx+1)}).appendTo(skill_div);
+            $("<a/>", {text: 'x',class: 'remove-skill-link'}).appendTo(skill_div);
+            $("<br/>").appendTo(skill_div);
+            skill_div.appendTo("#skills");
         } else if (hasClass(e.target, 'remove-skill-link')) {
-            $(e.target).prev().remove();
-            $(e.target).next().remove();
-            $(e.target).remove();
+            $(e.target).closest("div.skill").remove();
         }
     }, false);
 });
