5649612ae64cf97315e62bbc560bc7cfbe0cdbc2
Pouya Arvandian <pouya314@gmail.com>
2017-06-26 02:06:13 +1000

    increment index by looking at the max instead of last index

--- static/js/resume_builder.js
+++ static/js/resume_builder.js
@@ -11,9 +11,20 @@ $(function() {
     document.addEventListener('click', function (e) {
         e.preventDefault();
         if (hasClass(e.target, 'add-skill-link')) {
-            var idx = is_container_empty($("#skills")) ? -1 : parseInt($("#skills").find(".skill").last().find("input").attr("name").split("_").pop());
+            var idx;
+            if(is_container_empty($("#skills"))){
+                idx = 0;
+            } else {
+                var _arr = $("#skills")
+                    .find(".skill")
+                    .map(function() {
+                        return $(this).find("input").attr("name").split("_").pop();
+                    })
+                    .get();
+                idx = Math.max.apply(Math,_arr) + 1; // No need for parseInt(), retval is already a number
+            }
             var skill_div = $("<div/>", {class: "skill"});
-            $("<input/>", {type: 'text', name: 'skill_'+(idx+1)}).appendTo(skill_div);
+            $("<input/>", {type: 'text', name: 'skill_'+idx}).appendTo(skill_div);
             $("<a/>", {text: 'x',class: 'remove-skill-link'}).appendTo(skill_div);
             $("<br/>").appendTo(skill_div);
             skill_div.appendTo("#skills");
